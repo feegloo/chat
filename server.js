ef8cd9d012f1bb3c482b4b9a6acd1f3d05a45c5f
Aleksander Figiel <olek.figiel@gmail.com>
2020-09-25 02:28:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 sockets = []
 
-server = require('net').createServer(socket => {
+require('net').createServer(socket => {
   sockets.push(socket)
-
-  socket.on('data', data => {
-    for (s of sockets) {
-      if (s !== socket) s.write(data)
-    }
-  })
-
-  socket.on('end', () => {
-    const i = sockets.indexOf(socket)
-    sockets.splice(i, 1)
-  })
-})
-
-server.listen(3000)
-console.log('running TCP chat server. client: nc localhost 3000')
+  
+  socket.on('data', data => { for (s of sockets) if (s !== socket) s.write(data) })
+  socket.on('end', () => { sockets.splice(sockets.indexOf(socket), 1) })
+  
+}).listen(3000) && console.log('TCP chat server running. client: nc localhost 3000')
