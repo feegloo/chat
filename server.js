7dbee3e72dbc9bd8781a30f31eb91b3ee609d28e
Aleksander Figiel <olek.figiel@gmail.com>
2020-09-25 08:57:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,5 +5,4 @@ require('net').createServer(socket => {
   
   socket.on('data', data => { for (s of sockets) if (s !== socket) s.write(data) })
   socket.on('end', () => { sockets.splice(sockets.indexOf(socket), 1) })
-  
 }).listen(3000) && console.log('TCP chat server running. client: nc localhost 3000')
